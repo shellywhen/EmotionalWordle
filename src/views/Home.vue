690bad8d828f12eca8fb89ba2ee41eed29f0e44e
Cheol Su Jeon <jcs9312@gmail.com>
2021-03-28 12:21:20 +0900

    recommiting home vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,40 @@
 <template>
-  <div class="home"></div>
+  <div
+    class="image"
+    v-for="(image, imageIndex) in images"
+    :key="imageIndex"
+    @click="index = imageIndex"
+    :style="{ backgroundImage: 'url(' + image + ')', width: '780px', height: '520px' }"
+  ></div>
 </template>
 
+
 <script lang="ts">
-import { Vue } from "vue-class-component";
-export default class Home extends Vue {}
-</script>
+  import { Vue } from 'vue-class-component';
+  
+  export default class Home extends Vue {
+    public images: string[] = [
+          require('@/assets/gifs/poem1.gif'),
+          require('@/assets/gifs/poem2.gif'),
+          require('@/assets/gifs/poem3.gif'),
+          require('@/assets/gifs/poem4.gif'),
+          require('@/assets/gifs/poem6.gif'),
+          require('@/assets/gifs/poem7.gif'),
+          require('@/assets/gifs/poem8.gif'),
+          require('@/assets/gifs/poem9.gif'),
+          require('@/assets/gifs/poem10.gif'),
+        ];
+    public index = null;
+  }
+</script> 
+
+<style scoped>
+  .image {
+    float: left;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+    border: 1px solid #ebebeb;
+    margin: 5px;
+  }
+</style>
